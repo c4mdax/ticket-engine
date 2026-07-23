f25f7ab5e353cc51fe9e911af89e6fccea2ceded
Luis Angel Moreno Delgado <c4mdax@gmail.com>
2026-07-23 17:05:15 -0600

    Initial commit: Spring Boot skeleton

---
+++ src/main/java/com/portfolio/ticket_engine/TicketEngineApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.ticket_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/ticket_engine/TicketEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.ticket_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
